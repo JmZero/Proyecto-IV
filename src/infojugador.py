18ee7cc904fc8730d093fc63525fb231a6016363
JmZero <jmzero1308@gmail.com>
2018-10-14 19:15:41 +0200

    Añadidos Test close #5

---
+++ src/infojugador.py
@@ -0,0 +1,40 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+
+class InfoJugador:
+
+	def __init__(self):
+		with open('jugadores.json', 'r') as f:
+			self.ap = json.load(f)
+
+	# Función que devolverá el Id del jugador
+	def getBattleTag(self, user):
+		for i in self.ap:
+			if i["battletag"] == user:
+				return user
+			else:
+				return False
+
+	# Función que devolverá si el perfil es publico o privado
+	def isPerfilPublico(self, user):
+		try:
+			for i in self.ap:
+				if i["battletag"] == user:
+					return i["perfil"]
+				else:
+					return False
+		except:
+			return False
+
+	# Función que añadirá un nuevo jugador
+	def setJugador(self, user, perfil):
+		try:
+			self.ap.append({"battletag":user, "perfil":perfil})
+			with open('jugadores.json', 'w') as f:
+				json.dump(self.ap, f)
+			return True
+		except:
+			return False
---
+++ src/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+import infojugador
+
+class InfoJugadorTest(unittest.TestCase):
+
+    test = infojugador.InfoJugador()
+
+    def testBattleTag(self):
+        self.assertEqual(self.test.getBattleTag(1),False,"El battletag no puede ser un número")
+        self.assertEqual(self.test.getBattleTag("JmZero"),str('JmZero'),"El Battletag es correcto")
+
+    def testPerfilPublico(self):
+        self.assertEqual(self.test.isPerfilPublico("Antonio"),False,"El battletag existe")
+        self.assertEqual(self.test.isPerfilPublico("JmZero"),str('Público'),"El perfil es público")
+
+    def testAddUser(self):
+        self.assertEqual(self.test.setJugador("JuSan", "Público"),True,"El jugador se añadió correctamente")
+
+if __name__ == '__main__':
+    unittest.main()
