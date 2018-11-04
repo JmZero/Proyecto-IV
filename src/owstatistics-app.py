57c1bc13b7f4973dcb19b0fd82ec61abf754a06c
JmZero <jmzero1308@gmail.com>
2018-11-04 19:41:38 +0100

    Añadimos aplicación close #11

---
+++ src/owstatistics-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, jsonify
+import infojugador
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+	return jsonify(status="Ok")
+
+@app.route('/player/<battletag>')
+def Info(battletag):
+	p=funciones.InfoJugador()
+	datos=p.isPerfilPublico(battletag)
+	return jsonify(perfil=datos)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Error HTTP 404, Not Found'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
